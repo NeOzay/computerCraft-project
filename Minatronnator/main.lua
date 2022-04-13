7cbaefb45d5a2a2dcb3d65741a3226483610c058
NeOzay <colpaert.benoit@gmail.com>
2022-04-13 22:24:51 +0200

    pc

--- Minatronnator/main.lua
+++ Minatronnator/main.lua
@@ -18,36 +18,82 @@ local fuel_slot = 16
 
 local out_chest = side.top
 
-local width = 5
-local height = 5
+local width = 16
+local height = 16
+
+local function startCheck()
+	turtle.select(15)
+	local ender = turtle.getItemDetail()
+	turtle.select(16)
+	local coal = turtle.getItemDetail()
+	if not ender or ender.name ~= "enderstorage:ender_storage" or not coal or coal.name ~= "minecraft:coal" then
+		error("no enderchest in 15 slot and coal in 16 slot")
+	end
+end
 
 local function forward(dis)
-	for i = 1, dis or 1 do
+	for i = 2, dis or 2 do
 		turtle.digDown()
-		turtle.forward()
+		local ok, text = turtle.forward()
+		if not ok then
+			print(text)
+			os.sleep(2)
+			print("retry")
+			forward()
+		end
 	end
 end
 
----@param _side '"left"'|'"right"'
+---@param _side boolean
 local function half_turn(_side)
-	if _side == "left" then
+	if _side then
 		turtle.turnLeft()
 		forward()
 		turtle.turnLeft()
-	end
-
-	if _side == "right" then
+	else
 		turtle.turnRight()
 		forward()
 		turtle.turnRight()
 	end
 end
 
-for i = 1, math.floor(width/2) do
-	forward(height)
-	half_turn("left")
-	forward(height)
-	half_turn("right")
+local function empty()
+	turtle.select(15)
+	turtle.placeUp()
+	for i = 1, 14 do
+		turtle.select(i)
+		turtle.dropUp()
+	end
+	turtle.select(15)
+	turtle.digUp()
+	turtle.select(16)
 end
-turtle.down()
-turtle.digDown()
+
+local function refuel()
+	if turtle.getFuelLevel() < turtle.getFuelLimit() * 2.0 then
+		turtle.select(16)
+		local count = turtle.getItemCount()
+		if count > 1 then
+			turtle.refuel(count-1)
+		end
+	end
+end
+
+local sideflip = false
+startCheck()
+while true do
+	sideflip = false
+	for i = 1, width - 1 do
+		forward(height)
+		half_turn(sideflip)
+		sideflip = not sideflip
+		empty()
+	end
+	forward(height)
+	turtle.digDown()
+	turtle.turnRight()
+	turtle.down()
+	turtle.digDown()
+	refuel()
+	empty()
+end
